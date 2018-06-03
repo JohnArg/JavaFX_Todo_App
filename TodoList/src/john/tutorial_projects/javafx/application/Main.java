2ff4f37e62e0f6c198a155bb880726102f1c916b
John Argyroulis <johnargyroulis@gmail.com>
2018-06-03 17:56:00 +0300

    first commit

---
+++ TodoList/src/john/tutorial_projects/javafx/application/Main.java
@@ -0,0 +1,27 @@
+package john.tutorial_projects.javafx.application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("../ui_config/views/launch.fxml"));
+			
+			Scene scene = new Scene(root,500,500);
+			scene.getStylesheets().add(getClass().getResource("../ui_config/styles/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ TodoList/src/john/tutorial_projects/javafx/ui_config/UI_Controller.java
@@ -0,0 +1,5 @@
+package john.tutorial_projects.javafx.ui_config;
+
+public class UI_Controller {
+
+}
