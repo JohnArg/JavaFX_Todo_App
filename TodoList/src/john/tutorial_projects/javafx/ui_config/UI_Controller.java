b227b8799db1d0d2f5f54c4c6cb257efcedf932e
John Argyroulis <johnargyroulis@gmail.com>
2018-06-22 19:08:59 +0300

    Added Sorted List + Filtered List + Exit

--- TodoList/src/john/tutorial_projects/javafx/ui_config/UI_Controller.java
+++ TodoList/src/john/tutorial_projects/javafx/ui_config/UI_Controller.java
@@ -3,9 +3,16 @@ package john.tutorial_projects.javafx.ui_config;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -22,6 +29,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -45,11 +53,18 @@ public class UI_Controller {
 	private ContextMenu listContextMenu;
 	@FXML
 	private Button newBtn;
+	@FXML
+	private ToggleButton filterToggleBtn;
+	private FilteredList<TodoItem> filteredList;
+	private Predicate<TodoItem> allItems;
+	private Predicate<TodoItem> todayItems;
 	
 	@SuppressWarnings("unchecked")
 	public void initialize() {
+		//Add icon to Button manually
 		newBtn.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("icons/new_item24.png"))));
 		
+		//Use context menu to display a submenu with the Delete option when right clicking an item
 		listContextMenu = new ContextMenu();
 		MenuItem deleteMenuItem = new MenuItem("Delete");
 		deleteMenuItem.setOnAction(new EventHandler<ActionEvent>(){
@@ -61,8 +76,50 @@ public class UI_Controller {
 		});
 		listContextMenu.getItems().addAll(deleteMenuItem);
 		
-		todoListView.setItems(TodoData.getInstance().getTodoItems());
+		//Create a filtered list with today's items
+		//Initialize the predicates for the list
+		allItems = new Predicate<TodoItem>() {
+			@Override
+			public boolean test(TodoItem todoItem) {
+				return true;
+			}
+		};
+		todayItems = new Predicate<TodoItem>() {
+			@Override
+			public boolean test(TodoItem todoItem) {
+				return (todoItem.getDeadline().equals(LocalDate.now()));
+			}
+		};
+		filteredList = new FilteredList<TodoItem>(TodoData.getInstance().getTodoItems(), allItems);
+		
+		//Make the todos list sorted by date
+		SortedList<TodoItem> sortedList = new SortedList<TodoItem>(filteredList, 
+				new Comparator<TodoItem>() {
+						@Override
+						public int compare(TodoItem o1, TodoItem o2) {
+							return o1.getDeadline().compareTo(o2.getDeadline());
+						}
+				}
+		);
+		
+		//Populate Todo ListView and assign a cell to each Todo
+		//todoListView.setItems(TodoData.getInstance().getTodoItems());
+		todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
+
+			@Override
+			public void changed(ObservableValue<? extends TodoItem> observable, TodoItem oldValue, TodoItem newValue) {
+				if(newValue != null) {
+					TodoItem item = (TodoItem) todoListView.getSelectionModel().getSelectedItem();
+					descriptionArea.setText(item.getDetails());
+					DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+					dueLabel.setText(df.format(item.getDeadline()));
+				}
+			}
+			
+		});
+		todoListView.setItems(sortedList);
 		todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		todoListView.getSelectionModel().selectFirst();
 		todoListView.setCellFactory(new Callback< ListView<TodoItem>, ListCell<TodoItem> >() {
 			@Override
 			public ListCell<TodoItem> call(ListView<TodoItem> param) {
@@ -85,6 +142,7 @@ public class UI_Controller {
 					}
 				};
 				
+				//Bind the context menu to a non-null cell
 				cell.emptyProperty().addListener(
 						(obs, wasEmpty, isNowEmpty) -> {
 							if(isNowEmpty) {
@@ -122,16 +180,42 @@ public class UI_Controller {
 		    DialogController dgController = loader.getController();
 		    TodoItem newItem = dgController.processResults(); //adds it to the list and returns reference
 		    todoListView.getSelectionModel().select(newItem);
-		    onItemSelected();
+		}
+	}
+
+	@FXML
+	public void exitApp() {
+		Platform.exit();
+	}
+	
+	@FXML
+	public void handleKeyPressed(KeyEvent keyEvent) {
+		TodoItem selectedItem = (TodoItem) todoListView.getSelectionModel().getSelectedItem();
+		if(selectedItem != null) {
+			if(keyEvent.getCode().equals(KeyCode.DELETE)) {
+				deleteItem(selectedItem);
+			}
 		}
 	}
 	
 	@FXML
-	public void onItemSelected() {
-		TodoItem task = (TodoItem) todoListView.getSelectionModel().getSelectedItem();
-		descriptionArea.setText(task.getDetails());
-		DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
-		dueLabel.setText(df.format(task.getDeadline()));
+	public void hanldeFilterBtn() {
+		TodoItem item = (TodoItem) todoListView.getSelectionModel().getSelectedItem();
+		if(filterToggleBtn.isSelected()) {
+			filteredList.setPredicate(todayItems);
+			if(filteredList.isEmpty()) {
+				descriptionArea.clear();
+				dueLabel.setText("");
+			}else if(filteredList.contains(item)) {
+				todoListView.getSelectionModel().select(item);
+			}else {
+				todoListView.getSelectionModel().selectFirst();
+			}
+		}else {
+			//show all
+			filteredList.setPredicate(allItems);
+			todoListView.getSelectionModel().select(item);
+		}
 	}
 	
 	public void deleteItem(TodoItem item) {
@@ -142,15 +226,6 @@ public class UI_Controller {
 		Optional<ButtonType> result = alert.showAndWait();
 		if(result.isPresent() && (result.get() == ButtonType.OK)) {
 			TodoData.getInstance().deleteTodoItem(item);
-		}
-	}
-	
-	public void handleKeyPressed(KeyEvent keyEvent) {
-		TodoItem selectedItem = (TodoItem) todoListView.getSelectionModel().getSelectedItem();
-		if(selectedItem != null) {
-			if(keyEvent.getCode().equals(KeyCode.DELETE)) {
-				deleteItem(selectedItem);
-			}
 		}
 	}
 	
